aa5f784c880cf3923cfdbeaf76c62c62ef45fe9f
uday <udaypachauri969@gmail.com>
2026-08-13 15:48:49 +0530

    starting

--- uday.js
+++ uday.js
@@ -1,2 +1,48 @@
-udayydwvdggfg
-gudsfuyfgyud
+// This message will appear in browser console
+console.log("🔥 UDAY.JS IS CONNECTED!");
+
+// Change heading and paragraph
+function changeText() {
+
+    document.getElementById("title").innerHTML =
+        "JavaScript Successfully Connected!";
+
+    document.getElementById("message").innerHTML =
+        "Bhai tera JavaScript perfectly chal raha hai 🚀";
+}
+
+
+// Change background color
+function changeColor() {
+
+    document.body.style.backgroundColor = "lightblue";
+
+    document.getElementById("output").innerHTML =
+        "Background color changed using JavaScript!";
+}
+
+
+// Show current time
+function showTime() {
+
+    let currentTime = new Date();
+
+    document.getElementById("output").innerHTML =
+        "Current Time: " + currentTime.toLocaleTimeString();
+}
+
+
+// Click counter
+let clicks = 0;
+
+function countClick() {
+
+    clicks++;
+
+    document.getElementById("counter").innerHTML =
+        "Clicks: " + clicks;
+}
+
+
+// Extra test
+alert("js connected🚀");
